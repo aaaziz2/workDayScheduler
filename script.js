aacc5d1a9286c28f7656a14d03412a3ea90d0c11
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-14 20:11:49 -0500

    Removed p tag from save column Fixed javascript for formatting events based on time of day added event delegation so clicking on save button will save events Events are now saved to local storage

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var scheduleChart = document.querySelector("#chart")
-
+// var button = document.querySelectorAll(".saveBtn")
 var time = moment().format("H")
 
 console.log(time)
+// console.log(button)
 
 for(i = 0; i<scheduleChart.children.length; i++){
     var row = scheduleChart.children[i]
+    // console.log(row)
     var chartTime = row.children[0].children[0].textContent
     // https://stackoverflow.com/questions/1862130/strip-all-non-numeric-characters-from-string-in-javascript
     chartTime = chartTime.replace(/\D/g,'')
+    
     if(i>4){
         chartTime = Number(chartTime) + 12
     }
@@ -16,16 +19,33 @@ for(i = 0; i<scheduleChart.children.length; i++){
 
     var scheduleEvent = row.children[1]
     var timeDif = time - chartTime
-    // if(timeDif>0){
-    //     console.log(timeDif)
-    //     scheduleChart.classList.add("past")
-    // }
-    // else if(timeDif == 0){
-    //     console.log(timeDif)
-    //     scheduleChart.classList.add("present")
-    // }
-    // else if(timeDif<0){
-    //     console.log(timeDif)
-    //     scheduleChart.classList.add("future")
-    // }
+    if(timeDif>0){
+        scheduleEvent.classList.add("past")
+    }
+    else if(timeDif == 0){
+        scheduleEvent.classList.add("present")
+    }
+    else if(timeDif < 0){
+        scheduleEvent.classList.add("future")
+    }
 }
+
+function saveEvent(event){
+    event.preventDefault()
+    
+    if(event.target.classList.contains("saveBtn")){
+        var scheduledEvent = event.target.parentNode.children[1].children[0]
+        var scheduledEventTime = event.target.parentNode.children[0].children[0].textContent 
+        scheduledEventTime = scheduledEventTime.replace(/\D/g,'')
+        if(scheduledEventTime<8){
+            scheduledEventTime = Number(scheduledEventTime) + 12
+        }
+        // console.log(scheduledEventTime)
+        // console.log(test.value)
+        
+        localStorage.setItem(scheduledEventTime,scheduledEvent.value)
+    }
+    
+}
+
+scheduleChart.addEventListener('click',saveEvent)
