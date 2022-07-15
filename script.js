ae254b71457de7d1d9e53dca71199b49865ed191
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-14 20:31:39 -0500

    Added comments explaining my code

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+// selects the whole day scheduler chart
 var scheduleChart = document.querySelector("#chart")
-// var button = document.querySelectorAll(".saveBtn")
+// Current time by 24HR clock just the Hours
 var time = moment().format("H")
 
-console.log(time)
-// console.log(button)
+// time = time - 8
 
+// Testing for current time
+// console.log(time)
+
+// Checks all the existing timeslots
+//  Formats based on relation to the current time of day
+//  Adds the items that are saved to local storage
 for(i = 0; i<scheduleChart.children.length; i++){
+    // variable that represents the current row(timeslot) we are checking
     var row = scheduleChart.children[i]
-    // console.log(row)
+    // Grab the value of the time
     var chartTime = row.children[0].children[0].textContent
     // https://stackoverflow.com/questions/1862130/strip-all-non-numeric-characters-from-string-in-javascript
+    // Takes out the non-numeric chars
     chartTime = chartTime.replace(/\D/g,'')
-    
+    // after number five we add 12 to match the 24HR clock
     if(i>4){
         chartTime = Number(chartTime) + 12
     }
-    console.log(chartTime)
-
+    // console.log(chartTime)
+    // variable for the event section so we can add classlist as needed
     var scheduleEvent = row.children[1]
+    // check for past,current,future
     var timeDif = time - chartTime
+    // depending on the value add the appropriate class
     if(timeDif>0){
         scheduleEvent.classList.add("past")
     }
@@ -28,24 +38,32 @@ for(i = 0; i<scheduleChart.children.length; i++){
     else if(timeDif < 0){
         scheduleEvent.classList.add("future")
     }
+    // If localStorage/scheduled events exist, add them to the page
+    if(localStorage.getItem(chartTime)){
+        row.children[1].textContent = localStorage.getItem(chartTime)
+    }
 }
-
+// Save event if save button is pressed
 function saveEvent(event){
-    event.preventDefault()
-    
+    // Only save events if the save button is pressed  
     if(event.target.classList.contains("saveBtn")){
-        var scheduledEvent = event.target.parentNode.children[1].children[0]
+        // grabs What wwas entered in the scheduler
+        var scheduledEvent = event.target.parentNode.children[1].children[0].value
+        // grabs the associated time
         var scheduledEventTime = event.target.parentNode.children[0].children[0].textContent 
+        // stripping the non-numeric char so its easier to reference earlier
         scheduledEventTime = scheduledEventTime.replace(/\D/g,'')
+        // converting PM to 24HR
         if(scheduledEventTime<8){
             scheduledEventTime = Number(scheduledEventTime) + 12
         }
         // console.log(scheduledEventTime)
         // console.log(test.value)
-        
-        localStorage.setItem(scheduledEventTime,scheduledEvent.value)
+
+        // write item to local storage
+        localStorage.setItem(scheduledEventTime,scheduledEvent)
     }
     
 }
-
+// Event Delegation so I don't have to make an event listener for each button
 scheduleChart.addEventListener('click',saveEvent)
